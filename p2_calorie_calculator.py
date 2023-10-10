9df54abbcbd114566af0ad338738d83f4120c6db
ElisaRam <145053790+ElisaRam@users.noreply.github.com>
2023-10-09 19:46:54 -0600

    Create p2_calorie_calculator.py

---
+++ p2_calorie_calculator.py
@@ -0,0 +1,18 @@
+#https://replit.com/join/xoeangbkcj-elisaramirez4
+
+weight_kg = float(input("Enter your weight in kilograms: "))
+duration_minutes = int(input("Enter the duration of the activity in minutes: "))
+activity = input("Enter the type of activity (running, swimming, cycling): ").lower()
+
+if activity == "running":
+    calories_burned = 0.076 * weight_kg * duration_minutes
+elif activity == "swimming":
+    calories_burned = 0.068 * weight_kg * duration_minutes
+elif activity == "cycling":
+    calories_burned = 0.035 * weight_kg * duration_minutes
+else:
+    print("Invalid activity. Try again.")
+    calories_burned = None
+
+if calories_burned is not None:
+    print(f"You burned approximately {calories_burned} calories while {activity}.")
